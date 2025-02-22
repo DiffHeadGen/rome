b4d4e7815923c642e6bf3a754511f7e8f6380004
ceastld <1950946176@qq.com>
2025-02-22 17:41:40 +0000

    infer

--- create_env.sh
+++ create_env.sh
@@ -13,6 +13,9 @@ conda install -c fvcore -c iopath -c conda-forge fvcore iopath -y
 
 # 安装预编译的 pytorch3d
 pip install --no-index --no-cache-dir pytorch3d -f https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/py38_cu118_pyt200/download.html
+
+pip install -e ../expdata
+
 # ------------------------------------------------------------
 # download data
 # 创建目录并下载文件
