7758c2fcc8af659024804e13e32cf6a81d56f845
ceastld <1950946176@qq.com>
2025-02-22 14:48:58 +0000

    Update environment setup for PyTorch and pytorch3d installation

--- create_env.sh
+++ create_env.sh
@@ -1,6 +1,15 @@
-# conda deactivate
-# conda env remove -n rome -y
-conda create -n rome python=3.8 -y
-conda activate rome
+ml CUDA/11.8.0
+# conda create -n rome python=3.8 -y
+# conda activate rome
+
+# 先安装 PyTorch
+pip install torch torchvision --index-url https://download.pytorch.org/whl/cu118
+
+# 安装基础依赖
 pip install -r requirements.txt
-pip install git+https://github.com/facebookresearch/pytorch3d.git
+
+# 安装 pytorch3d 的系统依赖
+conda install -c fvcore -c iopath -c conda-forge fvcore iopath -y
+
+# 安装预编译的 pytorch3d
+pip install --no-index --no-cache-dir pytorch3d -f https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/py38_cu118_pyt200/download.html
